689be7c9546f2660d1e940bf36b32af9d6dabb64
Abhijit <abhijitm08@yahoo.co.uk>
2014-11-03 14:48:45 +0000

    Added a multiplication of 2 numbers in helloworld directory

---
+++ helloworld/ex2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+//This program multiplies two numbers
+double a;
+double b;
+
+std::cout << "Type the first number" << std::endl;
+std::cin >> a;
+std::cout << "Type the first number" << std::endl;
+std::cin >> b;
+std::cout << "The multiplication of a x b = " << a*b << std::endl;
+}
